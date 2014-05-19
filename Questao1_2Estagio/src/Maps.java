da17f22177448ab347bd41fa328e70945b763f36
Diogo <Diogo@Diogo-Notebook>
2014-05-19 15:15:44 -0300

    Concluido!

---
+++ Questao1_2Estagio/src/Maps.java
@@ -0,0 +1,15 @@
+
+public class Maps {
+	
+	//Resolução lista de exercicio 2 estagio: Diogo de Andrade e Flaviano Cézar
+	
+	/*
+	  	HashMap: Armazena o Map em uma hash table, a ordenação dos eus elementos não é garantida;
+
+		TreeMap: Implementação de um Map armazenado em uma estrutura de árvore, ordenação dos seus elementoes é garantida e de rápido retorno;
+
+		LinkedHashMap: Mantém uma lista ligada entre as entradas do Map, mantém a ordem de inserção dos elementos.
+	 */
+	
+
+}
