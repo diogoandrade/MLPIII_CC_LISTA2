c7d376d7709fd8fc8ce8edf92ebfdcc8fb4117a3
Diogo <Diogo@Diogo-Notebook>
2014-05-19 17:43:03 -0300

    Concluido!

---
+++ Questao2_2Estagio/src/br/unipe/mlpIII/questao2/questao2.java
@@ -0,0 +1,17 @@
+package br.unipe.mlpIII.questao2;
+
+public class questao2 {
+	
+	/*	Questao 3. Defina Test Driven Development (TDD) e descreva seu ciclo dedesenvolvimento.
+	 * 
+	 * 	É uma técnica de desenvolvimento de software que baseia em um ciclo curto de repetições:
+	 * 	Ciclo de desenvolvimento:
+	 * 	1. Adicione um teste;
+	 * 	2. Execute todos os testes e veja se algum deles falha;
+	 * 	3. Escrever código;
+	 * 	4. Execute os testes automatizados e veja-os executarem com sucesso;
+	 * 	5. Refatorar código;
+	 * 	6. Repita tudo.
+	 * */
+
+}
